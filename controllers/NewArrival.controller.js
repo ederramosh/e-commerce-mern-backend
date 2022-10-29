74386d6156d3a41327bf01ee47a0bc56d17327c8
Eder Ramos <eder.ramosh@gmail.com>
2022-10-28 21:36:52 -0600

    NewArrival modified

--- controllers/NewArrival.controller.js
+++ controllers/NewArrival.controller.js
@@ -44,10 +44,8 @@ const enterNewArrival = async (req, res) => {
 
 const getNewArrival = async (req, res) => {
     try {
-        //This option can filter the items in stock or  items that the store does not have it.
-        const { availability } = req.body;
 
-        const itemsInStock = await NewArrival.find({ availability: availability });
+        const itemsInStock = await NewArrival.find();
 
         if(itemsInStock.length === 0) {
             return res.status(404).json({
